6f5542011775c1764501c3a44c2c0c0159f1c494
RileyPetron36 <petroril000@rocori.k12.mn.us>
2025-11-04 12:57:55 -0600

    menu

---
+++ Food Emojis Table.py
@@ -0,0 +1,16 @@
+print ("welcome to Our Restaurant menu\n")
+
+print ("Appetizers:")
+print ("Garlic Bread 🍞: $5.99")
+print ("Bruschetta 🍕 : $6.99")
+print ("Mozzarella sticks 🧀: $7.99\n")
+
+print ("Main Courses:")
+print ("Spaghetti Bolognese 🍝: $12.99")
+print ("Chicken Alfredo 🍗: $14.99")
+print ("Steak with mashed potatoes 🥔🥩: $19.99\n")
+
+print ("Desserts:")
+print ("Tiramisu 🍰: $6.99 ")
+print ("Chocolate Cake 🍰🍫: $5.99")
+print ("Fruit Sald 🍓🍌: $4.99")
